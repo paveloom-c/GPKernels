7c285652ba6f29f00421c622a62e6d4f7ecec4a6
Pavel Sobolev <paveloom@mail.ru>
2020-08-25 17:11:56 +0300

    Change to `sympy`.

--- scripts/user/python/install-python-packages.sh
+++ scripts/user/python/install-python-packages.sh
@@ -2,5 +2,5 @@
 
 echo '\n\e[1;36mInstalling Python packages:\e[0m'
 
-echo '\e[1;36m> Installing `scipy`...\e[0m'
-pip3 install scipy
+echo '\e[1;36m> Installing `sympy`...\e[0m\n'
+pip3 install sympy
