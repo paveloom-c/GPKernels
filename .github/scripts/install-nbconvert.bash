c6cf226504f82d9cebd66984bfb48016a6cbdb34
Pavel Sobolev <paveloom@mail.ru>
2020-08-26 21:06:02 +0300

    Try with the `-y` flags.

--- .github/scripts/install-nbconvert.bash
+++ .github/scripts/install-nbconvert.bash
@@ -11,13 +11,13 @@ echo -e "\n${cyan}Updating the list of packages...${reset}\n"
 sudo apt-get update
 
 echo -e "\n${cyan}Installing `python3`...${reset}\n"
-sudo apt-get install python3-dev
+sudo apt-get install -y --no-install-recommends python3-dev
 
 echo -e "\n${cyan}Installing `pip3`...${reset}\n"
-sudo apt-get install python3-pip
+sudo apt-get install -y --no-install-recommends python3-pip
 
 echo -e "\n${cyan}Upgrading `pip3`...${reset}\n"
-python3 -m pip install --upgrade pip
+pip3 install --upgrade pip
 
 echo -e "\n${cyan}Installing `nbconvert`...${reset}\n"
 pip3 install --no-cache-dir nbconvert
