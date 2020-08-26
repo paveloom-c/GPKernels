543ff94c45377b79ff12dc5247b18a1a4df6a5de
Pavel Sobolev <paveloom@mail.ru>
2020-08-26 21:35:13 +0300

    Use the preinstalled `Python 3.8.1`.

--- .github/scripts/install-nbconvert.bash
+++ .github/scripts/install-nbconvert.bash
@@ -7,17 +7,5 @@
 cyan="\e[1;36m" # Bold cyan
 reset="\e[0m"   # Reset colors
 
-echo -e "\n${cyan}Updating the list of packages...${reset}\n"
-sudo apt-get update
-
-echo -e "\n${cyan}Installing `python3`...${reset}\n"
-sudo apt-get install -y --no-install-recommends python3-dev
-
-# echo -e "\n${cyan}Installing `pip3`...${reset}\n"
-# sudo apt-get install -y --no-install-recommends python3-pip
-
-# echo -e "\n${cyan}Upgrading `pip3`...${reset}\n"
-# pip3 install --upgrade pip
-
-# echo -e "\n${cyan}Installing `nbconvert`...${reset}\n"
-# pip3 install --no-cache-dir nbconvert
+echo -e "\n${cyan}Installing `nbconvert`...${reset}\n"
+pip3 install --no-cache-dir nbconvert
