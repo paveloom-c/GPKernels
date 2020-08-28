7d3dda44f4504c1d57bc53ddd17694784088d430
Pavel Sobolev <paveloom@mail.ru>
2020-08-28 20:53:11 +0300

    Update the base images and remove the workarounds.

--- main-scripts/user/python/install-python-packages.sh
+++ main-scripts/user/python/install-python-packages.sh
@@ -2,24 +2,14 @@
 
 echo '\n\e[1;36mInstalling Python packages:\e[0m'
 
+echo '\e[1;36m> Creating a hidden folder for the user...\e[0m'
+sudo mkdir -p /usr/other/$USER && sudo chgrp -R $USER /usr/other/$USER && sudo chmod -R g+w /usr/other/$USER
+
 echo '\e[1;36m> Installing `sympy`...\e[0m'
 pip3 install sympy >/dev/null 2>&1
 
-echo '\e[1;36m> Installing `kplr`...\e[0m\n'
+echo '\e[1;36m> Installing `kplr`...\e[0m'
 OTHER_PATH=/usr/other/$USER
-
-# Workaround until `binder-base` gets `zip`/`unzip`
-echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
-sudo apt-get update >/dev/null
-sudo apt-get install -y --no-install-recommends unzip >/dev/null
-echo 'debconf debconf/frontend select Dialog' | sudo debconf-set-selections
-sudo rm -rf /var/lib/apt/lists/*
-
-# Workaround until `binder-julia-plots` fixes
-# the lack of user rights to `/usr/other/$USER`
-sudo chgrp -R $USER $OTHER_PATH
-sudo chmod -R g+w $OTHER_PATH
-
 wget https://github.com/dfm/kplr/archive/main.zip -P $OTHER_PATH >/dev/null 2>&1
 unzip $OTHER_PATH/main.zip -d $OTHER_PATH >/dev/null
 rm $OTHER_PATH/main.zip
@@ -27,3 +17,6 @@ mv $OTHER_PATH/kplr-main $OTHER_PATH/kplr
 cd $OTHER_PATH/kplr
 sudo python3 $OTHER_PATH/kplr/setup.py install >/dev/null 2>&1
 cd - >/dev/null
+
+echo '\e[1;36m> Deleting the hidden folder...\e[0m\n'
+sudo rm -rf /usr/other/$USER
