e5b59aec08b68e8185c3d6748b19334565e548b1
Pavel Sobolev <paveloomm@gmail.com>
2020-08-30 16:20:39 +0300

    (#29) Add markdown lines:
    * Make the conversion script respect Markdown cells;
    * Define a set of Markdown lines for a preamble;
    * Define a set of Markdown lines for a kernel.

--- docs/convert.jl
+++ docs/convert.jl
@@ -25,6 +25,87 @@ end
 # Get path to the `make.jl` script
 make = joinpath(@__DIR__, "make.jl")
 
+# Determine the Markdown lines sets
+md_lines_preamble = Dict{Int, String}(
+    [
+        1 => """
+        Let's add a couple of packages. If you try this out, know that they take up a
+        pretty decent amount of memory.
+        """,
+
+        2 => """
+        Now let's get the data from the
+        [Kepler Archive](https://archive.stsci.edu/kepler/data_search/search.php).
+        To make it easier, let's use the Python's [`kplr`](https://github.com/dfm/kplr)
+        package:
+        """,
+
+        3 => """
+        It's useful to look at this data, but before that let's make the plots a little
+        prettier. It's never harmful.
+        """,
+
+        4 => """
+        Okay, now all the power of TeX is under our control. Let's make that plot then:
+        """,
+
+        5 => """
+        These sharp leaps are associated with the inconsistency of observational periods.
+        Let's pick one of the quarters:
+        """,
+
+        6 => """
+        That will be enough. Let's look at the time series:
+        """,
+
+        7 => """
+        Gaussian processes are cool, of course, but the models they will use will require
+        initial values. Some of these have already been identified before (mean and
+        variance), but we will also need an estimate of the period. Let's go the classic
+        way and make this estimate by determining the peak position on the Lomb-Scargle
+        periodogram.
+        """,
+
+        8 => """
+        Here's another look at it, just with a logarithmic scale:
+        """,
+
+        9 => """
+        Yeah, close the plot. It's just good practice. This ends the preamble to this
+        object, and we can now proceed with tests of different models for Gaussian
+        processes. Click on any of the model IDs in the sidebar to continue.
+        """,
+    ]
+)
+
+md_lines_kernel = Dict{Int, String}(
+    [
+        1 => """
+        Let's execute the preamble in the current scope. Text output will go here as a
+        brief reminder of the input data.
+        """,
+
+        2 => """
+        Let's define a function for calculating the negative log marginal likelihood and an
+        auxiliary function for unpacking the tuple of parameters. See the `IDs` page on the
+        sidebar for decrypting model IDs.
+        """,
+
+        3 => """
+        Initial values can be quite important in the optimization process, so it is
+        advisable to set them well. Fortunately, a small manual selection with creating the
+        plot can help in this. Let's try to choose such parameters that a realization
+        of the Gaussian process is similar to the original time series.
+        """,
+
+        4 => """
+        Finally, let's optimize the negative log marginal likelihood function. This process
+        can take quite a lot of time (it was likely aborted manually at some point), so the
+        output of the optimizer is very large and therefore placed under the spoiler.
+        """,
+    ]
+)
+
 # Go to the output directory
 generated_folder = joinpath(@__DIR__, "src", "generated")
 output_folder = joinpath(generated_folder, notebooks_folder_name)
@@ -37,18 +118,35 @@ for (index, notebook) in enumerate(notebooks)
     # Get the name of the notebook
     name = names[index]
 
+    # Initialize an empty Markdown lines set
+    md_lines = Dict{Int, String}()
+
     # Determine the preamble
     for preamble in preambles
         if name == preamble
+
+            # Create a working directory
             !isdir(name) && mkdir(name)
             cd(name)
+
+            # Choose a Markdown lines set
+            md_lines = md_lines_preamble
+
             break
+
         elseif count(preamble, notebook) == 1
+
+            # Create a working directory
             !isdir(preamble) && mkdir(preamble)
             cd(preamble)
             !isdir(name) && mkdir(name)
             cd(name)
+
+            # Choose a Markdown lines set
+            md_lines = md_lines_kernel
+
             break
+
         end
     end
 
@@ -67,30 +165,50 @@ for (index, notebook) in enumerate(notebooks)
     inside_output_block = false
     last_index = size(lines, 1)
 
-    # Put the text output in the text block
+    # Initialize the code block counter
+    code_block = 0
+
+    # Put the text output in the text blocks
     for (index, line) in enumerate(lines)
 
+        # Condition of entering the code block
         if startswith(line, "```julia") && !inside_julia_block
-            inside_julia_block = true
+
+            code_block += 1
+
+            # Condition to complete the output block
             if inside_output_block
                 lines[index - 1] = "```\n"
                 inside_output_block = false
             end
-            continue
+
+            # Insert a Markdown line if it is defined for the current cell
+            lines[index] = get(md_lines, code_block, "") * '\n' * lines[index]
+
+            inside_julia_block = true
+
+        # Condition of exiting the code block
         elseif startswith(line, "```") && inside_julia_block
+
             inside_julia_block = false
-            continue
-        end
 
-        if !inside_output_block && !inside_julia_block  && !isempty(line) #=
-        =# && !startswith(line, "![png]") && !startswith(line, "![svg]")
-            inside_output_block = true
+        # Condition to complete the output block
+        elseif inside_output_block && !isempty(line) && !startswith(line, "    ")
+
+            lines[index - 1] = "```\n"
+            inside_output_block = false
+
+        # Condition for starting the output block
+        elseif !inside_output_block && !inside_julia_block  && startswith(line, "    ")
+
             lines[index - 1] = "\n```text\n"
-            continue
-        end
+            inside_output_block = true
 
-        if inside_output_block && index == last_index
+        # Condition on the last line of the last block of output
+        elseif inside_output_block && index == last_index
+
             lines[index] = "\n```"
+
         end
 
     end
@@ -115,3 +233,6 @@ for (index, notebook) in enumerate(notebooks)
     cd(output_folder)
 
 end
+
+# Go back to the current directory
+cd(@__DIR__)
